7e474b5327937d07542608ff18fc7bf8ca0fc1ee
Jaykant Adjariya <jaykant.adjariya@nextuple.com>
2023-03-29 09:47:59 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Suspense, lazy } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { ReactRouterGlobalNavigation } from './helpers/ReactRouterGlobalNavigation';
+// import { getSession } from './helpers/localStorageHelper';
+
+const WelcomeScreen = lazy(() => import('./screens/WelcomeScreen'));
+
+const ProtectedLayout = () => {
+  // if (!getSession()) {
+  //   return redirectToAuthLoginPage();
+  // }
+
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<WelcomeScreen />} />
+        <Route exact path="/hello" element={<div>hello worl</div>} />
+      </Routes>
+    </>
+  );
+};
+
+// const PublicLayout = () => {
+//   return (
+//     <>
+//       <Switch />
+//     </>
+//   );
+// };
+
+const App = () => {
+  return (
+    <HashRouter>
+      <div>
+        <ReactRouterGlobalNavigation />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="*" element={<ProtectedLayout />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import store from './app/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import './styles/index.css';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
